e198c6bd094250ddc8f72f121ee12d57220053c4
LincolnMcloud <lamcloudjr@gmail.com>
2022-06-15 13:43:42 -0400

    fourth commit

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get("/", (req,res)=>{
-    console.log("Hi")
-    res.json("Hi")
+app.post("/", (req,res)=>{
+    console.log(req.body)
+    res.json(req.body)
 })
 
 app.get("/test", (req,res)=>{
